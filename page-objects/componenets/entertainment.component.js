496cd9a2d41a280430bc42fed10397b8edf212b4
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-09-26 09:44:30 +0300

    Before rewrite framework's method clickOnReferences...

--- page-objects/componenets/entertainment.component.js
+++ page-objects/componenets/entertainment.component.js
@@ -4,8 +4,5 @@ class EntertainmentComponenet {
     constructor() {
         this.entertainment = element(by.css('a[class*=entertainment]'));
     }
-    async goToEntertainmentPage() {
-        return await this.entertainment.click();
-    }
 }
 module.exports = EntertainmentComponenet;
